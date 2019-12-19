15a6406e9fa79307db297fcbc7cf11ebbf931923
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-19 17:21:05 +0300

    100

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-var num = [];
-var fizz = 'Fizz';
-var buzz = 'Buzz'; 
-
 for (var i = 0; i <= 100; i++) {
-    if (i % 3 == 0) {
-        num.push(fizz);
-    } else if (i % 5 == 0 & i % 3 !== 0) {
-        num.push(buzz);
-    } else {
-      num.push(i);  
-    }
+    console.log(i);
 }
-console.log(num);
